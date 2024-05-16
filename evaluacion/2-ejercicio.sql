ca1d7c1210794a3549e675046305ecd4026ea0fd
valentinadutoya <laravalentinadutoya@gmail.com>
2024-05-16 11:30:15 -0300

    ejercicio 2 listo

--- evaluacion/2-ejercicio.sql
+++ evaluacion/2-ejercicio.sql
@@ -3,4 +3,7 @@
 -- todos las mujeres de menos de 40 años, 
 -- elegir el orden en que se muestran 
 
-
+SELECT name , car_make, car_model, plate_number FROM drivers_license d
+JOIN  person p ON p.license_id = d.id 
+WHERE age < 40 AND gender = 'female'
+ORDER BY name;
