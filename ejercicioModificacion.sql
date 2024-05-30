088538089d5f9f1af05871f3ef4d1331687a054c
valentinadutoya <laravalentinadutoya2019@gmail.com>
2024-05-30 09:50:50 -0300

    inserte canciones a un album

---
+++ ejercicioModificacion.sql
@@ -0,0 +1,11 @@
+/*1*/
+INSERT INTO artists(name)
+VALUES ("Miranda");
+
+INSERT INTO albums(Title,ArtistId)
+VALUES ("es mentira ",276);
+
+INSERT INTO tracks (name,MediaTypeId,Milliseconds,UnitPrice)
+VALUES ("Mentira",2,208800,0.99),
+	   ("Nada es igual",3,199800,0.99),
+	   ("Iman",4,153600,0.99);
